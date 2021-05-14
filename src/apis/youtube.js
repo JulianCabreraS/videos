4f66f5d861f421a272504ed8e35b9659503aee1f
jcabrera <julian.cabreras@gmail.com>
2021-05-14 17:21:15 -0500

    1. Video Search App

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+const KEY = "AIzaSyCRLjQdmmW8zalIvSS2HUts_tzNRhpNQSs";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 5,
+    key: KEY,
+  },
+});
