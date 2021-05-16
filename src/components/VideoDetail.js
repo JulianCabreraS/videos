bde8189b197f1bfaeeafd826aaa36a5ff2d46c90
jcabrera <julian.cabreras@gmail.com>
2021-05-15 20:10:56 -0500

    2. VideoDetail component created

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{video.snippet.title}</div>;
+};
+
+export default VideoDetail;
